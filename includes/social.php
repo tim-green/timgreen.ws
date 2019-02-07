2cfad7368745707e3e593bd5769f4b3fce6ae008
Tim <tim@timgreen.ws>
2019-02-07 16:54:13 +1100

    Update social.php

--- includes/social.php
+++ includes/social.php
@@ -3,9 +3,6 @@
          <a href="https://github.com/tim-green" target="_blank">
           <i class="fab fa-github"></i>
         </a>
-        <a href="http://www.linkedin.com/in/thetimgreen" target="_blank">
-          <i class="fab fa-linkedin"></i>
-        </a>
         <a href="https://www.instagram.com/somewhatcreative/" target="_blank">
           <i class="fab fa-instagram"></i>
         </a>
