4283e25d85063b94cafbcfb888f5186e2aa9478e
Tim <tim@timgreen.ws>
2019-05-05 22:18:13 +1000

    updated experience

--- includes/experience.php
+++ includes/experience.php
@@ -3,6 +3,14 @@
       <h2 class="now wow fadeInUp">Experience</h2>
       <p class="wow fadeInUp">
         <summary class="experience-info">
+          <span class="experience-info-comapny">372 Digital</span>
+          <span class="experience-info-position">Website Developer</span>
+          <span class="experience-info-years">2019 - Current</span>
+        </summary>
+      </p>
+    
+      <p class="wow fadeInUp">
+        <summary class="experience-info">
           <span class="experience-info-comapny">2B Advertising and Design</span>
           <span class="experience-info-position">Website Developer</span>
           <span class="experience-info-years">2011-2018</span>
