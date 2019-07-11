e76309f262df7331589c0d3ea40c0fe36a7b5b35
Tim <github@timgreen.ws>
2019-07-11 20:58:29 +1000

    move freelance to top of experience list

--- includes/experience.php
+++ includes/experience.php
@@ -1,27 +1,20 @@
  <section id="exp" class="">
     <div class="container ">
       <h2 class="now wow fadeInUp">Experience</h2>
-      <p class="wow fadeInUp">
-        <summary class="experience-info">
-          <span class="experience-info-comapny">372 Digital</span>
-          <span class="experience-info-position">Website Developer</span>
-          <span class="experience-info-years">2019 - Current</span>
-        </summary>
-      </p>
-    
-      <p class="wow fadeInUp">
-        <summary class="experience-info">
-          <span class="experience-info-comapny">2B Advertising and Design</span>
-          <span class="experience-info-position">Website Developer</span>
-          <span class="experience-info-years">2011-2018</span>
-        </summary>
-      </p>
     
       <p class="wow fadeInUp">
         <summary class="experience-info">
           <span class="experience-info-comapny">Freelancer</span>
           <span class="experience-info-position">Website Developer</span>
-          <span class="experience-info-years">2010</span>
+          <span class="experience-info-years">2010 - Present</span>
+        </summary>
+      </p>
+
+      <p class="wow fadeInUp">
+        <summary class="experience-info">
+          <span class="experience-info-comapny">2B Advertising and Design</span>
+          <span class="experience-info-position">Website Developer</span>
+          <span class="experience-info-years">2011-2018</span>
         </summary>
       </p>
 
