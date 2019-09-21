d5b52731eca44baa42c6b73fd5702a26b8e6e72a
Tim <github@timgreen.ws>
2019-09-21 11:26:18 +1000

    updated style to minifcation version

--- react/index.php
+++ react/index.php
@@ -26,7 +26,7 @@
 
   <!-- CSS -->
   <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-LRlmVvLKVApDVGuspQFnRQJjkv0P7/YFrw84YYQtmYG4nK8c+M+NlmYDCv0rKWpG" crossorigin="anonymous">
-  <link rel="stylesheet" href="../build/css/styles.css" >
+  <link rel="stylesheet" href="../build/css/styles.min.css" >
   <link rel="stylesheet" href="../assets/css/react.css" >
 </head>
 
