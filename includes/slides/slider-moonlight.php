8063e652e435bb09f6e6d82a7a88d7a3a7d4b1b6
Tim <github@timgreen.ws>
2019-10-13 16:19:59 +1100

    added moonlight slider file

---
+++ includes/slides/slider-moonlight.php
@@ -0,0 +1,17 @@
+      <h3>Moonlight - One Page Drupal Theme</h3>
+      <div class="slider slider-moon">
+        <div class="slide">
+          <figure>
+            <img class="slide-image" src="assets/images/slide-MoonlightDrupal.jpg" alt="">
+            <figcaption class="slide-caption">Moonlight Drupal Theme</figcaption>
+          </figure>
+        </div>       
+      </div>
+      <div class="work-details">
+        <p>
+          <sup class="site-credit">Simple one page Drupal theme build for showcasing an mobile app</sup>
+        </p>
+        <a href="https://drupal.timgreen.ws/" rel="follow">View website</a>
+          <span class="bullet-separator project-sep">&bull;</span>
+        <a href="https://github.com/tim-green/moonlight-drupal-theme" target="_blank" rel="nofollow">GitHub</a> 
+      </div>
