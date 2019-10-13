dd42a0283962a335344dd3ada8b13d736cd934cc
Tim <github@timgreen.ws>
2019-10-13 16:19:53 +1100

    added idle slider file

---
+++ includes/slides/slider-idle.php
@@ -0,0 +1,23 @@
+      <h3>Idle - Multipage Site Template</h3>
+      <div class="slider slider-idle">
+        <div class="slide">
+          <figure>
+            <img class="slide-image" src="assets/images/slide-IdleTemplate1.jpg" alt="">
+            <figcaption class="slide-caption">Idle Site Template - Mockup</figcaption>
+          </figure>
+        </div>
+        <div class="slide">
+          <figure>
+            <img class="slide-image" src="assets/images/slide-IdleTemplate2.jpg" alt="">
+            <figcaption class="slide-caption">Idle Site Template - Mockup</figcaption>
+          </figure>
+        </div>
+      </div>
+      <div class="work-details">
+        <p>
+          <sup class="site-credit">Just a mockup of an mutli-page with blog site layout</sup>
+        </p>
+        <a href="https://www.timgreen.ws/idle/" rel="follow">View website</a>
+          <span class="bullet-separator project-sep">&bull;</span>
+        <a href="https://github.com/tim-green/idle" target="_blank" rel="nofollow">GitHub</a> 
+      </div>
