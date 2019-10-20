6e034f346a6bf86759f826b0864e9b77952d1027
Tim <github@timgreen.ws>
2019-10-20 21:46:25 +1100

    created night owl slider include file

---
+++ includes/slides/slider-nightowl.php
@@ -0,0 +1,35 @@
+      <h3>Night Owl - Bootstrap Admin Dashboard Template</h3>
+      <div class="slider slider-swc">
+        <div class="slide">
+          <figure>
+            <img class="slide-image" src="assets/images/slide-NightOwl.jpg" alt="">
+            <figcaption class="slide-caption">Night Owl - Bootstrap Admin Dashboard Template</figcaption>
+          </figure>
+        </div>
+        <div class="slide">
+          <figure>
+            <img class="slide-image" src="assets/images/slide-NightOwl2.jpg" alt="">
+            <figcaption class="slide-caption">Night Owl - Bootstrap Admin Dashboard Template</figcaption>
+          </figure>
+        </div>
+        <div class="slide">
+          <figure>
+            <img class="slide-image" src="assets/images/slide-NightOwl3.jpg" alt="">
+            <figcaption class="slide-caption">Night Owl - Bootstrap Admin Dashboard Template</figcaption>
+          </figure>
+        </div>
+          <div class="slide">
+          <figure>
+            <img class="slide-image" src="assets/images/slide-NightOwl4.jpg" alt="">
+            <figcaption class="slide-caption">Night Owl - Bootstrap Admin Dashboard Template</figcaption>
+          </figure>
+        </div>
+      </div>
+      <div class="work-details">
+        <p>
+          <sup class="site-credit">Night Owl is a Bootstrap 4 based fully responsive admin dashboard template. Night Owl been based on a simple and modular design, which allows it to be easily customised. Night Owl comes with lots of reusable and beautiful UI elements, widgets.</sup>
+        </p>
+        <a href="https://night-owl.timgreen.ws/" rel="follow">View website</a>
+          <span class="bullet-separator project-sep">&bull;</span>
+        <a href="https://github.com/tim-green/night-owl-dashboard" target="_blank" rel="nofollow">GitHub</a> 
+      </div>
