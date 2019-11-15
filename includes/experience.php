283f89e0cbe625fe21c3f90059d64f92060e5fbf
Tim <github@timgreen.ws>
2019-11-15 17:11:59 +1100

    uncommented freelance experience

--- includes/experience.php
+++ includes/experience.php
@@ -2,14 +2,13 @@
     <div class="container ">
       <h2 class="now wow fadeInUp">Experience</h2>
     
-      <?php /*<p class="wow fadeInUp">
+      <p class="wow fadeInUp">
         <summary class="experience-info">
-          <span class="experience-info-comapny">Freelancer</span>
+          <span class="experience-info-comapny">Freelance</span>
           <span class="experience-info-position">Website Developer</span>
           <span class="experience-info-years">2019 - Current</span>
         </summary>
-      </p>*/
-      ?>
+      </p>
 
       <p class="wow fadeInUp">
         <summary class="experience-info">
