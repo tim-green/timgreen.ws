0dbf9b17a02c5fe65896083ccc619b888aa0087a
Tim <github@timgreen.ws>
2020-04-05 17:04:43 +1000

    updated gruntfile to include sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,59 +1,85 @@
-module.exports = function(grunt){
-    
-    grunt.initConfig({
-      concat: {
-        js: {
-          src: ['assets/js/jquery.js', 'assets/js/pace.js', 'assets/js/wow.min.js', 'assets/js/slick.min.js', 'assets/js/typed.min.js','assets/js/main.js','assets/js/scroll-to-top.js'],
-          dest: 'build/js/scripts.js',  
-        },
-        css: {
-          src: ['assets/css/fonts.css', 'assets/css/style.css','assets/css/dark-mode.css', 'assets/css/slick.css'],
-          dest: 'build/css/styles.css',
-        },
-      },
-      watch: {
+'use strict';
+module.exports = function(grunt) {
+  grunt.initConfig({
+    concat: {
       js: {
-        files: ['assets/js/*.js'],
-        tasks: ['concat'],
-      },
-      css: {
-        files: ['assets/css/*.css'],
-        tasks: ['concat'],
-      },
-      minjs: {
-        files: ['build/js/scripts.js'],
-        tasks: ['uglify'],
-      },
-      mincss: {
-        files: ['build/css/styles.css'],
-        tasks: ['cssmin'],
+        src: ['assets/js/jquery.js', 'assets/js/pace.js', 'assets/js/wow.min.js', 'assets/js/slick.min.js', 'assets/js/typed.min.js','assets/js/main.js','assets/js/scroll-to-top.js'],
+        dest: 'assets/js/scripts.js',  
       },
     },
-    // minify js configuration. - grunt uglify
-      uglify: {
-        minifyscripts: {
-          files: {
-            'build/js/scripts.min.js': ['build/js/scripts.js'],
-          }
+    sass: {
+      dist: {
+        options: {
+          style: 'compressed',
+          compass: false,
+          sourcemap: false
+        },
+        files: {
+          'build/css/app.min.css': [
+              'assets/sass/app.scss'
+          ]
         }
-      },
-    // minify css configuration. - grunt cssmin
-    cssmin: {
-      minifystyles: {
-        files: [{
-          expand: true,
-          cwd: 'build/css',
-          src: ['styles.css'],
-          dest: 'build/css',
-          ext: '.min.css'
-        }]
       }
+    },
+    uglify: {
+      dist: {
+        files: {
+          'build/js/app.min.js': [
+            'assets/js/scripts.js'
+          ]
+        },
+        options: {
+          sourceMap: 'build/js/app.min.js.map',
+          sourceMappingURL: 'build/js/app.min.js.map'
+        }
+      }
+    },
+    watch: {
+      options: {
+        livereload: true
+      },
+      sass: {
+        files: [
+          'assets/sass/**/*.scss'
+        ],
+        tasks: ['sass']
+      },
+      js: {
+        files: [
+          'assets/js/**/*.js'
+        ],
+        tasks: ['concat','uglify']
+      },
+      html: {
+        files: [
+          '**/*.html'
+        ]
+      }
+    },
+    clean: {
+      dist: [
+        'build/css/app.min.css',
+        'build/js/app.min.js'
+      ]
     }
-//above this    
-    });
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.registerTask('default',['concat','uglify','cssmin','watch']);
-}
+  });
+
+  // Load tasks
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  // Register tasks
+  grunt.registerTask('default', [
+    'clean',
+    'concat',
+    'sass',
+    'uglify'
+  ]);
+  grunt.registerTask('dev', [
+    'watch'
+  ]);
+
+};
