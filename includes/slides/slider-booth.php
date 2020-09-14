c4bd8a757b7e2f5ec9ed52b4048458a0dd7eb5c3
Tim <github@timgreen.ws>
2020-09-14 14:13:48 +1000

    created new slider php for booth portfolio item

---
+++ includes/slides/slider-booth.php
@@ -0,0 +1,30 @@
+ <?php /* booth woocommerce theme slider*/ ?>
+      <h3>Booth - WooCommerce Theme</h3>
+      <div class="slider slider-moon">
+        <div class="slide">
+          <figure>
+            <img class="slide-image" src="assets/images/slide-booth.jpg" alt="">
+            <figcaption class="slide-caption">Booth WooCommerce Theme</figcaption>
+          </figure>
+        </div>  
+        <div class="slide">
+          <figure>
+            <img class="slide-image" src="assets/images/slide-boothblog.jpg" alt="">
+            <figcaption class="slide-caption">Booth WooCommerce Theme</figcaption>
+          </figure>
+        </div>
+        <div class="slide">
+          <figure>
+            <img class="slide-image" src="assets/images/slide-boothmobile.jpg" alt="">
+            <figcaption class="slide-caption">Booth WooCommerce Theme</figcaption>
+          </figure>
+        </div>  
+      </div>
+      <div class="work-details">
+        <p>
+          <sup class="site-credit">WooCommerce theme for WordPress. Simple and easy to use and fully responsive.</sup>
+        </p>
+        <a href="https://themes.timgreen.ws/booth/" rel="follow">View website</a>
+          <span class="bullet-separator project-sep">&bull;</span>
+        <a href="https://github.com/tim-green/booth-woocommerce-theme" target="_blank" rel="nofollow">GitHub</a> 
+      </div>
