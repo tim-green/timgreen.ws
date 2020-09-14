84528e49377aff29adaaa6b155b233c71b4405d7
Tim <github@timgreen.ws>
2020-09-14 14:14:27 +1000

    added booth slider include and removed old projects

--- includes/work.php
+++ includes/work.php
@@ -4,16 +4,17 @@
     <div class="container">
       <h2 class="now wow fadeInUp">Selected work</h2>
       <?php 
-        include('includes/slides/slider-solarhub.php'); 
+        //include('includes/slides/slider-solarhub.php'); 
         include('includes/slides/slider-rslnational.php');
-        include('includes/slides/slider-strathnairn.php');
+        //include('includes/slides/slider-strathnairn.php');
         include('includes/slides/slider-crossfit.php'); 
-        include('includes/slides/slider-atsb.php'); 
-        include('includes/slides/slider-afma.php'); 
+        //include('includes/slides/slider-atsb.php'); 
+        //include('includes/slides/slider-afma.php'); 
         include('includes/slides/slider-orb.php');
         include('includes/slides/slider-idle.php');
         include('includes/slides/slider-moonlight.php');
         include('includes/slides/slider-nightowl.php');
+        include('includes/slides/slider-booth.php');
         include('includes/slides/slider-retroreplay.php');
       ?>
     </div>
