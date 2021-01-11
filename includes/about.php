94a43987ecca458063ab00ded709960e5718fb0b
Tim <github@timgreen.ws>
2021-01-11 14:14:59 +1100

    update about info with automotiv

--- includes/about.php
+++ includes/about.php
@@ -9,11 +9,16 @@
         I worked with some great agencies, government organisations, individuals and start-ups to their reach both business and website goals.
 
        My approach is realistic and creative, supported by skills in web development such as using <span class="skill">HTML</span>, <span class="skill">CSS/Sass</span>, <span class="skill">PHP</span>, <span class="skill">JavaScript/jQuery</span> and along with using frameworks like <span class="skill">Bootstrap</span>.</p>
+
        <p class="wow fadeInUp">
         I have experience using <span class="skill">WordPress</span>, <span class="skill">Drupal</span>, <span class="skill">Shopify</span> and <span class="skill">WooCommerce</span>, I can also create <span class="skill">Accessible PDF and Word Documents</span>. I can create and manage email campaigns using <span class="skill">MailChimp</span>. </p>
 
       <p class="wow fadeInUp">
-        I also have experience with the design and video software from <span class="skill">Adobe Creative Cloud</span>, and I use <span class="skill">GruntJS</span> to automatically perform frequent tasks that I use on a daily basis when building websites and I use <a href="https://github.com/tim-green" rel="nofollow"><span class="skill">GitHub</span></a> and <span class="skill">Fork</span> to help with version control my code.
+        I also have experience with the design and video software from <span class="skill">Adobe Creative Cloud</span>, and I use <span class="skill">GruntJS</span> to automatically perform frequent tasks that I use on a daily when building websites and I use <a href="https://github.com/tim-green" rel="nofollow"><span class="skill">GitHub</span></a> and <span class="skill">Fork</span> to help with version control my code.
+      </p>
+
+      <p class="wow fadeInUp">
+      Every week I use a social media management tool called <span class="skill">HootSuite</span> where I create and schedule social media posts to <a href="https://www.facebook.com/SomeWhat-Creative-1654165058227710/" rel="nofollow">Facebook</a> and <a href="https://www.instagram.com/somewhatcreative/" rel="nofollow">Instagram</a> about the upcoming posts on my blog <a href="https://www.somewhatcreative.net" rel="nofollow">SomeWhat Creative</a>.
       </p>
 
       <h2 class="now wow fadeInUp">What I'm currently learning</h2>
@@ -26,7 +31,7 @@
       I'm currently working on developing some of my own <span class="skill">WordPress plugins</span>, I've developed one plugin called Greeny Cookie Consent, and this plugin is simple and developer-friendly plugin that let visitors know that the site is using cookies. You can view the <a href="//github.com/tim-green/greeny-cookie-consent" rel="nofollow"><span class="skill">GitHub Repo</span></a> where's the plugin is hosted.</p>
       
       <p class="now wow fadeInUp">
-        And also I've created a simple, clean and responsive WooCommerce specific theme called <span class="skill">Booth</span> which you can <a href="//themes.timgreen.ws/booth/" rel="nofollow"><span class="skill">visit the site here</span></a>, or view the <a href="//github.com/tim-green/booth-woocommerce-theme" rel="nofollow"><span class="skill">GitHub Repo</span></a>, also I've created my first <a href="//www.npmjs.com/package/tim-green-resume" rel="nofollow"><span class="skill">NPM package</span></a> which is based on my resume.
+        And also I'm in the progress in developing a clean responsive WooCommerce theme called <span class="skill">Automotiv</span> which you can <a href="//themes.timgreen.ws/automotiv/" rel="nofollow"><span class="skill">visit the site here</span></a>, or view the <a href="//github.com/tim-green/automotiv-wp-theme" rel="nofollow"><span class="skill">GitHub Repo</span></a>, also I've created my first <a href="//www.npmjs.com/package/tim-green-resume" rel="nofollow"><span class="skill">NPM package</span></a> which is based on my resume.
       </p>
     </div>
   </section>
