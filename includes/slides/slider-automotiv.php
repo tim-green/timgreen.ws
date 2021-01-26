84a134dda614300c29fef7713b003bde00868c9a
Tim <github@timgreen.ws>
2021-01-26 17:11:45 +1100

    created new slider include for automotiv

---
+++ includes/slides/slider-automotiv.php
@@ -0,0 +1,28 @@
+<?php /* Automotiv Slider*/ ?>
+      <h3>Automotiv</h3>
+      <div class="slider slider-swc">
+        <div class="slide">
+          <figure>
+            <img class="slide-image" src="assets/images/slide-automotiv-home-faq.jpg" alt="">
+            <figcaption class="slide-caption">Automotiv - WordPress &amp; WooCommerce Theme</figcaption>
+          </figure>
+        </div>
+        <div class="slide">
+          <figure>
+            <img class="slide-image" src="assets/images/slide-automotiv-store.jpg" alt="">
+            <figcaption class="slide-caption">Automotiv - WordPress &amp; WooCommerce Theme</figcaption>
+          </figure>
+        </div>
+        <div class="slide">
+          <figure>
+            <img class="slide-image" src="assets/images/slide-automotiv-video-post.jpg" alt="">
+            <figcaption class="slide-caption">Automotiv - WordPress &amp; WooCommerce Theme</figcaption>
+          </figure>
+        </div>
+      </div>
+      <div class="work-details">
+        <p>
+          <sup class="site-credit">Automotiv is a WordPress &amp; WooCommerce theme built using WooCommerce and Advanced Custom Fields as it core plugins. </sup>
+        </p>
+        <a href="https://themes.timgreen.ws/automotiv/" rel="follow">View website</a>
+      </div>
