a28a7f7c1238e247017ea6fe5b2ff90cfca686f8
Tim <github@timgreen.ws>
2021-07-19 14:34:43 +1000

    updated about info

--- includes/about.php
+++ includes/about.php
@@ -3,7 +3,7 @@
     <div class="container">
       <h2 class="now wow fadeInUp">Bio + Skills</h2>
       <p class="wow fadeInUp">
-        I'm a web developer with a passion for strong, simple websites. I have 12 years of experience working in both an advertising agency and freelance capacity, mainly focusing on building websites/web applications which are simple to use and look great.
+      I'm a web developer with a passion for strong, simple websites. I have 13 years of experience working in both an advertising agency and a freelance capacity, mainly focusing on building websites/web applications that are simple to use and look great.
       </p>
       <p class="wow fadeInUp">
         I worked with some great agencies, government organisations, individuals and start-ups to their reach both business and website goals.
@@ -14,7 +14,7 @@
         I have experience using <span class="skill">WordPress</span>, <span class="skill">Drupal</span>, <span class="skill">Shopify</span> and <span class="skill">WooCommerce</span>, I can also create <span class="skill">Accessible PDF and Word Documents</span>. I can create and manage email campaigns using <span class="skill">MailChimp</span>. </p>
 
       <p class="wow fadeInUp">
-        I also have experience with the design and video software from <span class="skill">Adobe Creative Cloud</span>, and I use <span class="skill">GruntJS</span> to automatically perform frequent tasks that I use on a daily when building websites and I use <a href="https://github.com/tim-green" rel="nofollow"><span class="skill">GitHub</span></a> and <span class="skill">Fork</span> to help with version control my code.
+        I also have experience with the design and video software from <span class="skill">Adobe Creative Cloud</span>, and I use <span class="skill">GruntJS</span> to automatically perform frequent tasks that I use on a daily basis when building websites and I use <a href="https://github.com/tim-green" rel="nofollow"><span class="skill">GitHub</span></a> and <span class="skill">Fork</span> to help with version control my code.
       </p>
 
       <p class="wow fadeInUp">
@@ -28,7 +28,7 @@
 
       <h2 class="now wow fadeInUp">What I'm currently building</h2>
       <p class="now wow fadeInUp">
-      I'm currently working on developing some of my own <span class="skill">WordPress plugins</span>, I've developed one plugin called Greeny Cookie Consent, and this plugin is simple and developer-friendly plugin that let visitors know that the site is using cookies. You can view the <a href="//github.com/tim-green/greeny-cookie-consent" rel="nofollow"><span class="skill">GitHub Repo</span></a> where's the plugin is hosted.</p>
+      I'm currently working on developing some of my own <span class="skill">WordPress plugins</span>, I've developed one plugin called Greeny Cookie Consent, and this plugin is a simple and developer-friendly plugin that let visitors know that the site is using cookies. You can view the <a href="//github.com/tim-green/greeny-cookie-consent" rel="nofollow"><span class="skill">GitHub Repo</span></a> where's the plugin is hosted.</p>
       
       <p class="now wow fadeInUp">
         And also I'm in the progress in developing a clean responsive WooCommerce theme called <span class="skill">Automotiv</span> which you can <a href="//themes.timgreen.ws/automotiv/" rel="nofollow"><span class="skill">visit the site here</span></a>, or view the <a href="//github.com/tim-green/automotiv-wp-theme" rel="nofollow"><span class="skill">GitHub Repo</span></a>, also I've created my first <a href="//www.npmjs.com/package/tim-green-resume" rel="nofollow"><span class="skill">NPM package</span></a> which is based on my resume.
